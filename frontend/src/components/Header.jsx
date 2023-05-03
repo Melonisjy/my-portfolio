0d3ebc7a35d905e6c7fd6518c5c638e085b1ab21
Melonisjy <dhy2457@naver.com>
2023-05-03 23:49:35 +0900

    🔨 refactor: "코드 위치 변경"

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -10,18 +10,17 @@ function Header() {
           <MdEmail size={28} />
           dhy2458@gmail.com
         </div>
-
         <ul className="flex text-xs md:text-base gap-2 md:gap-8 mr-20">
-          <Link to="/project">
-            <li className="flex flex-row items-center opacity-50 hover:opacity-100 font-koreanHeader">
-              <MdInsertDriveFile size={28} />
-              포트폴리오
-            </li>
-          </Link>
           <Link to="/">
             <li className="flex flex-row items-center opacity-50 hover:opacity-100 font-koreanHeader">
               <AiFillHome size={28} />
               메인화면
+            </li>
+          </Link>
+          <Link to="/project">
+            <li className="flex flex-row items-center opacity-50 hover:opacity-100 font-koreanHeader">
+              <MdInsertDriveFile size={28} />
+              포트폴리오
             </li>
           </Link>
         </ul>
